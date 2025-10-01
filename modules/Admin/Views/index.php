0244cef9f2fc4c0c80f455eff9d123c76f108007
Emanuelluz <emanueluz@gmail.com>
2025-10-01 14:54:23 -0300

    Corrige a configuração da rede interna no Docker Compose e remove texto desnecessário da mensagem de boas-vindas no módulo Admin

--- modules/Admin/Views/index.php
+++ modules/Admin/Views/index.php
@@ -5,7 +5,7 @@
     <title>Blog Module</title>
 </head>
 <body>
-    <h1>Welcome to the Admin Module! Mais um teste</h1>
+    <h1>Welcome to the Admin Module! </h1>
     <p>This is a modular component in CodeIgniter 4.6 PHP 8.3.</p>
 </body>
 </html>
