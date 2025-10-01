de7549513009ffd8e1da6afa842995c6d8ff8a34
Emanuelluz <emanueluz@gmail.com>
2025-10-01 13:15:46 -0300

    Rename module folder from acme/Blog to modules/Admin and update namespaces and routes accordingly

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('blog', ['namespace' => 'Modules\Admin\Controllers'], static function ($routes) {
+    $routes->get('/', 'Blog::index');
+});
---
+++ modules/Admin/Views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog Module</title>
+</head>
+<body>
+    <h1>Welcome to the Blog Module!</h1>
+    <p>This is a modular component in CodeIgniter 4.</p>
+</body>
+</html>
