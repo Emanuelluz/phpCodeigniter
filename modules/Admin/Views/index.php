af832c904de937524e5537f5aa38647a752145f4
Emanuelluz <emanueluz@gmail.com>
2025-10-01 13:39:37 -0300

    Atualiza a mensagem de boas-vindas no módulo Blog

--- modules/Admin/Views/index.php
+++ modules/Admin/Views/index.php
@@ -5,7 +5,7 @@
     <title>Blog Module</title>
 </head>
 <body>
-    <h1>Welcome to the Blog Module!</h1>
+    <h1>Welcome to the Blog Module show!</h1>
     <p>This is a modular component in CodeIgniter 4.</p>
 </body>
 </html>
