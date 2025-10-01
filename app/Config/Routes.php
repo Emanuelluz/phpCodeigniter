024848d94a6b5512929bee4d00ece25a18861bbf
Emanuelluz <emanueluz@gmail.com>
2025-10-01 15:43:08 -0300

    Atualiza a rota principal para direcionar para o controlador 'Modules\Admin\Controllers\Admin::index'.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Modules\Admin\Controllers\Admin::index');
 
 $routes->group('admin', ['namespace' => 'Modules\Admin\Controllers'], static function ($routes) {
     $routes->get('/', 'Admin::index');
