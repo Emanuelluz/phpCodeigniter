e2c556e01d611716ea0937597481211ee2eba6a8
Emanuelluz <emanueluz@gmail.com>
2025-10-02 09:00:18 -0300

    Ajusta formatação do texto no index.php para melhor legibilidade

--- modules/Admin/Views/index.php
+++ modules/Admin/Views/index.php
@@ -8,12 +8,12 @@
     <h1>Welcome to the Admin Module! </h1>
     
     Caso ainda use a imagem local sem registry:
-Eu já adicionei o placement constraint no serviço app para rodar no manager (onde a imagem é criada pelo runner). Isso evita inconsistências quando não há registro de imagens.
-Sobre “parar, limpar e subir nova versão”:
-Seu workflow já foi atualizado para:
-remover o stack anterior,
-rebuildar a imagem com pull,
-e redeployar o stack.
-Assim, mudanças no index.php passam a reflet
+    Eu já adicionei o placement constraint no serviço app para rodar no manager (onde a imagem é criada pelo runner). Isso evita inconsistências quando não há registro de imagens.
+    Sobre “parar, limpar e subir nova versão”:
+    Seu workflow já foi atualizado para:
+    remover o stack anterior,
+    rebuildar a imagem com pull,
+    e redeployar o stack.
+    Assim, mudanças no index.php passam a reflet
 </body>
 </html>
